726b661b46ae8418b11cf0d4e5218abacf15e543
musaliyah <asmusaliar@gmail.com>
2022-10-31 14:34:45 -0700

    resolvers.js additions

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -4,6 +4,35 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async(parent)
+        me: async(parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({
+                    _id: context.user._id
+                }).select('-_v-password');
+                return userData
+            }
+            throw new AuthenticationError('Please log in.')
+        },
+    },
+    Mutation: {
+        addUse: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({});
+            if(!user) {
+                throw new AuthenticationError('No user found');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw){
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async(parent, {newBook})
     }
 }
